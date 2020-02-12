149e7526c43035ecf3e8e7eb4c4b37cca40d9799
Dan Foley <dan@dan-foley.com>
2020-02-12 01:52:40 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -46,8 +46,7 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    binding.pry
-  find_by_name(name) ? find_by_name(name) : create(name)
+    find_by_name(name) ? find_by_name(name) : create(name)
   end
 
 end
