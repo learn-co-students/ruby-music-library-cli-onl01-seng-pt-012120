fb1b3fb4d3dc9696bb4db049b599dceace0c8752
Derric Fragosa <fderric@gmail.com>
2020-03-03 15:39:02 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+require 'pry'
+class Song 
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+   
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self 
+  end 
+  
+  def self.destroy_all
+    @@all.clear 
+  end 
+  
+  def self.create(song)
+    song = self.new(name)
+    song.save
+    song
+  end 
+  
+  
+end
