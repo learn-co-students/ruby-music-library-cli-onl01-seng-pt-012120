07c56092ec06cc0ae0b4acb6a941149a3f7b6394
Derric Fragosa <fderric@gmail.com>
2020-03-06 20:37:06 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,11 +1,11 @@
 module Concerns::Findable
   
   def find_by_name(name)
-    Song.all.find {|song| song.name == name}
+    self.all.find {|x| x.name == name}
   end
   
   def find_or_create_by_name(name)
-    find_by_name(name) || name = Song.new(name)
+    self.find_by_name(name) || name = Song.new(name)
    end
   
 end
