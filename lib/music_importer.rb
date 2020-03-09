91212ab562de04ecf450d62bfb4ef4fb4105829b
Derric Fragosa <fderric@gmail.com>
2020-03-09 16:51:42 +0000

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -17,7 +17,6 @@ class MusicImporter
   end
   
   def import
-  
-   files.each {|file| Song.create_from_filename(file)}
+    files.each {|file| Song.create_from_filename(file)}
   end
 end
---
+++ lib/music_library_controller.rb
@@ -0,0 +1,30 @@
+class MusicLibraryController
+  
+  def initialize (path= './db/mp3s')
+    MusicImporter.new(path).import
+  end
+  
+  def call
+    puts "Welcome to your music library!"  
+    puts "To list all of your songs, enter 'list songs'."
+    list_songs = gets.chomp
+    puts "To list all of the artists in your library, enter 'list artists'."
+    list_artists = gets.chomp
+    puts "To list all of the genres in your library, enter 'list genres'."
+    list_genres = gets.chomp
+    puts "To list all of the songs by a particular artist, enter 'list artist'."
+    list_artist = gets.chomp
+    puts "To list all of the songs of a particular genre, enter 'list genre'."
+    list_genre = gets.chomp
+    puts "To play a song, enter 'play song'."
+    play_song = gets.chomp
+    puts "To quit, type 'exit'." 
+    exit = gets.chomp
+    puts "What would you like to do?"
+  end
+  
+  def call_loops
+    
+  end
+
+end
